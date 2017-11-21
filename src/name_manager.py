a88f2d89d45c222e13a25e5fe86c93192478319c
Carlos <c.pramil@udc.es>
2017-11-21 03:20:42 +0100

    Apply license to some files

--- src/name_manager.py
+++ src/name_manager.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2017 Carlos Pérez Ramil <c.pramil at udc.es>
+
+# This file is part of ASP-Graph.
+
+# ASP-Graph is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# ASP-Graph is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with ASP-Graph.  If not, see <http://www.gnu.org/licenses/>.
+
 from ast import literal_eval
 
 class Singleton:
